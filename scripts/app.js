a2d2259c354ed176b7a74f7b8f4481f219962573
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2019-08-01 16:56:10 -0700

    adds some stuff

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,28 @@
 console.log('uwu')
 
 
+// --------------------------  Functions -------------------------- //
+
+// const changeVisibility = (event) => {
+//     if (event.target.class === )
+// }
 
 // ------------------------ Event Listeners ------------------------ //
 
+
+
+// getElementsByClassName('content-image').addEventListener('click', (event) => {
+//     console.log(event);
+// });
+
+// window.addEventListener('click', (event) => {
+//     console.log(event.target)
+// });
+
+document.getElementsByClassName(".content-image").addEventListener('click', (event) => {
+    console.log(event.target);
+});
+
+// window.addEventListener('click', (event) => {
+//     changeVisibility(event);
+// });
