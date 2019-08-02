1cc776ff8b716d4fa76f54dfcab222f4bf8b06ce
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2019-08-02 16:02:14 -0700

    idk

--- scripts/app.js
+++ scripts/app.js
@@ -1,28 +1,42 @@
-console.log('uwu')
-
-
-// --------------------------  Functions -------------------------- //
-
-// const changeVisibility = (event) => {
-//     if (event.target.class === )
-// }
-
-// ------------------------ Event Listeners ------------------------ //
-
-
-
-// getElementsByClassName('content-image').addEventListener('click', (event) => {
-//     console.log(event);
-// });
-
-// window.addEventListener('click', (event) => {
-//     console.log(event.target)
-// });
-
-// document.getElementsByClassName(".content-image").addEventListener('click', (event) => {
-//     console.log(event.target);
-// });
-
-window.addEventListener('click', (event) => {
-    changeVisibility(event);
+document.getElementById("modal-toggle").addEventListener("click", function() {
+    element = document.getElementById('modal').classList
+    element.toggle("hidden");
 });
+
+let imageCheckboxes = document.querySelectorAll('.checkbox-input');
+
+for (let i =0; i < imageCheckboxes.length; i++) {
+    imageCheckboxes[i].addEventListener("click", function(e) {
+        e.preventDefault()
+    })
+}
+
+
+
+// Validate Form Inputs
+const form = document.querySelector('form');
+form && form.addEventListener('submit', (e) => {
+    [...document.querySelectorAll('mce-error-response')].forEach(alert => {
+      alert.parentNode.removeChild(alert);
+    });
+    
+    [...form.elements].forEach(input => {
+      if (input.type !== 'submit' && input.value === '') {
+        e.preventDefault();
+        input.classList.add('input-error');
+        input.insertAdjacentHTML('afterend', `
+          <div class="alert alert-${input.id}">
+            Please enter your ${input.placeholder}
+          </div>
+        `);
+      }
+    });
+  });
+  
+  
+  // Clear Form Errors on Focus
+  document.addEventListener('focus', (e) => {
+    e.target.classList.remove('mce-error-response');
+    const inputMessage = document.querySelector(`.alert-${e.target.id}`);
+    inputMessage && inputMessage.parentNode.removeChild(inputMessage);
+  }, true);
